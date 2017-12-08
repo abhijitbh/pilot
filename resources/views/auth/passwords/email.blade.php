fdf9abd4cc201f6f6a115e8468245ee8cbbfe1af
Abhijit <abhijitbh.aloha@gmail.com>
2017-12-08 21:02:53 +0530

    updated UI

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,17 +5,31 @@
 @stop
 
 @section('content')
+    
+    <div class="row">
+        <div class="row vertical-align"> <!--
+                        ^--  Additional class -->    
 
         {!! Form::open(['url' => url('/password/email'), 'class' => 'form-signin' ] ) !!}
 
         @include('includes.status')
-
-        <h2 class="form-signin-heading">Password Reset</h2>
+        <div class="log-in-title" style="font-size: 40px;">Password Reset</div>
         <label for="inputEmail" class="sr-only">Email address</label>
-        {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email address', 'required', 'autofocus', 'id' => 'inputEmail' ]) !!}
+        {!! Form::email('email', null,
+        ['class' => 'form-control', 
+        'placeholder' => 'Email address', 
+        'required', 
+        'autofocus', 
+        'id' => 'inputEmail' ]) !!}
 
         <br />
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Send me a reset link</button>
+        <button class="btn btn-primary btn-round btn-lg btn-block register-btn" type="submit">Send me a reset link</button>
+        <div class="new-user" style="font-size: 25px;">or?
+                <a class="signup-link" href="/login" style="font-size: 25px;">Sign In</a>
+        </div>
+        </div>
+        </div>
+
 
         {!! Form::close() !!}
 
